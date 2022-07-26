d371c6af77a45854a918771aeada804417d85d67
dnjoe96 <donjoedbest@gmail.com>
2022-07-26 10:14:08 -0700

    decode auth header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 from api.v1.auth.auth import Auth
 from flask import request
 from typing import List, TypeVar
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,16 @@ class BasicAuth(Auth):
         if authorization_header[:6] != 'Basic ':
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """ The function decodes the Authorization in header """
+        if not base64_authorization_header:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            string = base64_authorization_header.encode('utf-8')
+            dcode = base64.decodebytes(string)
+        except Exception:
+            return None
+        return dcode.decode()
