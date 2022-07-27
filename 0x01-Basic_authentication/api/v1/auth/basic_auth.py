e75cfa8665b2f3d00203f09edc1ec649a9a16dd9
dnjoe96 <donjoedbest@gmail.com>
2022-07-26 20:40:12 -0700

    Load user, accommodate : in password

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ from api.v1.auth.auth import Auth
 from flask import request
 from typing import List, TypeVar
 import base64
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -33,3 +34,30 @@ class BasicAuth(Auth):
         except Exception:
             return None
         return dcode.decode()
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        """ Extract user detail from auth header """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        email = decoded_base64_authorization_header.split(':')[0]
+        pwd = ''.join(decoded_base64_authorization_header.split(':')[1:])
+        return email, pwd
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """ The function get user objects from"""
+        if not user_email or not user_pwd:
+            return None
+        if type(user_email) is not str or type(user_pwd) is not str:
+            return None
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
