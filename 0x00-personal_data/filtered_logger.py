44a85f035b9c4e0efe19b287fa0613782a4dc058
dnjoe96 <donjoedbest@gmail.com>
2022-08-06 23:35:08 +0100

    Remove print statement

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,9 +8,9 @@ def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """ returns the log message obfuscated """
     temp = message
-    print(temp)
+    # print(temp)
     for field in fields:
-        print(field)
+        # print(field)
         temp = re.sub(field + "=.*?" + separator,
                       field + "=" + redaction + separator, temp)
     return temp
