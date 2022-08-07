4d739b8c7cda69deaa84ef96e2410864ec55ad26
dnjoe96 <donjoedbest@gmail.com>
2022-08-07 08:19:14 +0100

    Add table Schema

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+    def __repr__(self):
+        return '<User(id=\'%s\', email=\'%s\')>' % (
+            self.id, self.email
+        )
