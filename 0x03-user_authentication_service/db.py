9214ef1b55b7848f260ca77eda0fc6e82bde84dc
dnjoe96 <donjoedbest@gmail.com>
2022-08-07 19:25:07 +0100

    Update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -40,7 +40,7 @@ class DB:
         return user
 
     # @staticmethod
-    def find_user_by(self, **vals: dict) -> User:
+    def find_user_by(self, **vals) -> User:
         """ find a user by an arbitrary attribute """
         try:
             user = self._session.query(User).filter_by(**vals).first()
@@ -49,3 +49,15 @@ class DB:
         if user is None:
             raise NoResultFound
         return user
+
+    def update_user(self, user_id: int, **vars) -> None:
+        """ method to update a user and save in db"""
+        try:
+            user = self.find_user_by(id=user_id)
+            for key, value in vars.items():
+                setattr(user, key, value)
+        except Exception:
+            raise Exception
+
+        self._session.commit()
+        return None
