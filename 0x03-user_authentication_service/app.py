101aad09d1143fbb39e3e7e8ccf201937c2333a4
dnjoe96 <donjoedbest@gmail.com>
2022-08-08 08:31:01 +0100

    Implement login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Flask App Module """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 from auth import Auth
 
 app = Flask(__name__)
@@ -29,6 +29,23 @@ def users():
         return jsonify({
             'message': 'email already registered'
         }), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session = AUTH.create_session(email)
+    resp = make_response('store session')
+    resp.set_cookie('session_id', session)
+    return jsonify({
+        'email': email,
+        'message': 'logged in',
+    })
 
 
 if __name__ == '__main__':
