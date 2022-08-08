17a828d3ec26e8ed9bbaba19655918553cf0d50b
dnjoe96 <donjoedbest@gmail.com>
2022-08-08 18:28:55 +0100

    Implement user profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -62,7 +62,18 @@ def logout():
         user = AUTH.get_user_from_session_id(session_id)
         if user:
             AUTH.destroy_session(user.id)
-            return redirect('/')
+            return redirect(url_for('index'))
+    abort(403)
+
+
+@app.route('/profile', methods=["GET"], strict_slashes=False)
+def profile():
+    """Finds user from session id"""
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+
     abort(403)
 
 
