20f52aaf957cc4fa42e18d77430c73b3e0e8081c
dnjoe96 <donjoedbest@gmail.com>
2022-09-10 12:05:33 +0100

    Task 5 - cookie authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -29,10 +29,12 @@ if auth:
 @app.before_request
 def just_before_request():
     """ Function implements before every request """
-    dlist = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    dlist = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/',
+             '/api/v1/auth_session/login/']
     if auth:
         if auth.require_auth(request.path, dlist):
-            if not auth.authorization_header(request):
+            if not auth.authorization_header(request) and \
+                    auth.session_cookie(request):
                 abort(401)
             if not auth.current_user(request):
                 abort(403)
