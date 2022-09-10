6c85d2e2238335c295debfa04d0582f5fd9d95c1
dnjoe96 <donjoedbest@gmail.com>
2022-09-10 12:31:37 +0100

    Initialize user

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -36,6 +36,10 @@ def view_one_user(user_id: str = None) -> str:
     if user_id == 'me' and request.current_user:
         return jsonify(request.current_user)
 
+    user = User.get(user_id)
+
+    if user is None:
+        abort(404)
     return jsonify(user.to_json())
 
 
