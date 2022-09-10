497adb22e0c9f2192233a27ecaecf27c16f30e79
dnjoe96 <donjoedbest@gmail.com>
2022-09-10 13:19:45 +0100

    Task 8

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -41,3 +41,16 @@ class SessionAuth(Auth):
         if user_id is None:
             return None
         return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """Deletes a user session/logout.
+        """
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+
+        if self.user_id_for_session_id(session_id) is None:
+            return False
+
+        del self.user_id_by_session_id[session_id]
+        return True
